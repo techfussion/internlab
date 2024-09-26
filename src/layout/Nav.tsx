a653b2cef1e7844dd157f123bb29f03e62813874
techfussion <muminraj15@gmail.com>
2024-09-26 11:39:50 +0100

    nav layout

---
+++ src/layout/Nav.tsx
@@ -0,0 +1,17 @@
+const Nav = () => {
+    return (
+        <nav className="flex">
+            <h1>InternLab</h1>
+            <ul>
+                <li>Find placement</li>
+                <li>Browse companies</li>
+            </ul>
+            <div>
+                <button>Login</button>
+                <button>Sign up</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
