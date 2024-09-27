312e4e6f442b7273b924b97c2bbd863cbdfb65cc
techfussion <muminraj15@gmail.com>
2024-09-27 17:16:39 +0100

    added more sections to landing page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,14 +4,19 @@ export interface ButtonProps {
     startIcon?: React.ReactNode;
     endIcon?: React.ReactNode;
     clear?: boolean;
+    fill?: string;
+    color?: string;
     text: string;
     type?: "button" | "submit" | "reset";
     onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick, startIcon, endIcon, type, clear }) => {
+const Button: React.FC<ButtonProps> = ({ text, onClick, startIcon, endIcon, type, clear, fill, color }) => {
+    fill = fill ? fill : 'bg-customBlue1';
+    color = color ? color : 'text-white';
+
     return (
-        <button className={`${clear ? 'text-textBlue1': 'bg-customBlue1 text-white'} text-xs py-2 px-5`} type={type} onClick={onClick}>{startIcon ? startIcon: ''}{text} {endIcon ? endIcon: ''}</button>
+        <button className={`${clear ? 'text-textBlue1': `${fill} ${color}`} text-xs py-2 px-5`} type={type} onClick={onClick}>{startIcon ? startIcon: ''}{text} {endIcon ? endIcon: ''}</button>
     )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,8 @@ import amd from "../assets/logo/amd-logo-1.svg";
 import talkit from "../assets/logo/talkit 1.svg";
 import { category } from "../global/constants";
 import { icons } from "../global/imageUtil";
+import Button from "../components/Button";
+import Footer from "../layout/Footer";
 
 
 const Home: React.FC = () => {
@@ -17,7 +19,7 @@ const Home: React.FC = () => {
             <div className="bg-customGray1 pb-10">
                 <Nav />
                 <section className="flex justify-center flex-col px-16 mt-8">
-                    <h1 className="text-5xl text-textDarkBlue1 leading-tight">Discover <br /> more than <br /> <span className="text-textBlue2">5000+ Placements</span></h1>
+                    <h1 className="text-5xl text-textDarkBlue1 leading-tight font-bold">Discover <br /> more than <br /> <span className="text-textBlue2">5000+ Placements</span></h1>
                     <img src={Dashes} alt="line" className="w-96"/>
                     <p className="opacity-70 text-sm text-textGray3 w-4/12 my-3">Great platform for the job seeker that searching for new career heights and passionate about startups.</p>
                     <SearchBar />
@@ -38,7 +40,7 @@ const Home: React.FC = () => {
             </section>
             <section className="px-16 mt-8 pb-10">
                 <div className="flex justify-between items-center">
-                    <h2 className="text-3xl text-textDarkBlue1">Explore by <span className="text-textBlue2">category</span></h2>
+                    <h2 className="text-3xl text-textDarkBlue1 font-bold">Explore by <span className="text-textBlue2">category</span></h2>
                     <div className="flex gap-2">
                         <p className="text-xs text-textBlue1 cursor-pointer">Show all</p>
                         <img src={icons.arrowRightBlue} alt="arrow" className="w-2"/>
@@ -48,7 +50,7 @@ const Home: React.FC = () => {
                     {category.map((item, index) => (
                         <div key={index} className="flex flex-col my-5 p-4 border w-56 cursor-pointer hover:scale-105">
                             <img src={item.img} alt="icon" className="w-8"/>
-                            <h4 className="font-semiboldtext-base text-textDarkBlue1 mt-3 mb-2">{item.name}</h4>
+                            <h4 className="font-semibold text-base text-textDarkBlue1 mt-3 mb-2">{item.name}</h4>
                             <div className="flex gap-2 items-center">
                                 <p className="text-xs opacity-50">Explore category</p>
                                 <img src={icons.arrowRight} alt="arrow" className="w-2"/>
@@ -57,6 +59,35 @@ const Home: React.FC = () => {
                     ))}
                 </div>
             </section>
+            <section className="px-16 mt-8 pb-10">
+                <div className="bg-customBlue1 flex py-10 h-96 flex items-center justify-evenly">
+                    <div className="text-white">
+                        <h2 className="text-3xl leading-tight font-bold">Recommend  <br />placements easily</h2>
+                        <p className="text-xs my-4 w-6/12 font-normal">Recommend companies that are not availalbe in our database, rate, and leave feedback for others.</p> 
+                        <Button text="Recommend" fill="bg-white" color="text-textBlue1" onClick={() => {}}/>
+                    </div>
+                    <img className="relative right-0" src="https://picsum.photos/200/300" alt="image"/>               
+                </div>
+            </section>
+            <section className="px-16 mt-8 pb-10">
+                <div className="flex justify-between items-center">
+                    <h2 className="text-3xl text-textDarkBlue1 font-bold">Popular <span className="text-textBlue2">companies</span></h2>
+                    <div className="flex gap-2">
+                        <p className="text-xs text-textBlue1 cursor-pointer">Show all</p>
+                        <img src={icons.arrowRightBlue} alt="arrow" className="w-2"/>
+                    </div>
+                </div>
+            </section>
+            <section className="px-16 mt-8 pb-10">
+                <div className="flex justify-between items-center">
+                    <h2 className="text-3xl text-textDarkBlue1 font-bold">Popular <span className="text-textBlue2">roles</span></h2>
+                    <div className="flex gap-2">
+                        <p className="text-xs text-textBlue1 cursor-pointer">Show all</p>
+                        <img src={icons.arrowRightBlue} alt="arrow" className="w-2"/>
+                    </div>
+                </div>
+            </section>
+            <Footer />
         </>
     )
 }
